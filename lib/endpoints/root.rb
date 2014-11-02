b5c34e68a5048fa7a47a9188567d625290c03d30
Noah Zoschke <nzoschke@gmail.com>
2014-11-02 15:38:20 -0800

    fix up how credentials are persisted in the session

--- lib/endpoints/root.rb
+++ lib/endpoints/root.rb
@@ -4,10 +4,11 @@ module Endpoints
   class Root < Base
     get "/" do
       # Display Rdio and Spotify info or initiate OAuth flow
+      redirect "/auth/rdio" unless rdio_credentials = env['rack.session']['rdio_credentials']
 
-      unless rdio_access_token = env['rack.session'][:rdio_access_token]
-        redirect "/auth/rdio"
-      end
+      # TODO: Get the comsumer object from OmniAuth somehow?
+      rdio_consumer = OAuth::Consumer.new(ENV['RDIO_APP_KEY'], ENV['RDIO_APP_SECRET'], { site: 'http://api.rdio.com' })
+      rdio_access_token = OAuth::AccessToken.new(rdio_consumer, rdio_credentials['token'], rdio_credentials['secret'])
 
       r = JSON.parse(rdio_access_token.post('http://api.rdio.com/1/',
         method: 'getPlaylists',
@@ -17,12 +18,12 @@ module Endpoints
     end
 
     get "/clear" do
-      env['rack.session'][:rdio_access_token] = nil
+      env['rack.session'].clear
       redirect "/"
     end
 
     get "/auth/rdio/callback" do
-      env['rack.session'][:rdio_access_token] = request.env['omniauth.auth']['extra']['access_token']
+      env['rack.session']['rdio_credentials'] = request.env['omniauth.auth']['credentials'].to_h
       redirect "/"
     end
   end

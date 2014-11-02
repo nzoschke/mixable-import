da43fccac2139a2768c9d21104c77d2b69cdd737
Noah Zoschke <nzoschke@gmail.com>
2014-11-02 12:35:23 -0800

    pliny-new; pliny-generate model playlist; lots of homebrew and ruby hacking to make it work

---
+++ db/migrate/1414960082_create_playlists.rb
@@ -0,0 +1,9 @@
+Sequel.migration do
+  change do
+    create_table(:playlists) do
+      uuid         :uuid, default: Sequel.function(:uuid_generate_v4), primary_key: true
+      timestamptz  :created_at, default: Sequel.function(:now), null: false
+      timestamptz  :updated_at
+    end
+  end
+end

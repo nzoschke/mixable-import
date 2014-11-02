e0657a348eebdc3b3544166d131b260f998453eb
Noah Zoschke <nzoschke@gmail.com>
2014-11-02 14:40:04 -0800

    rdio OAuth flow session kind of working!

--- lib/endpoints/root.rb
+++ lib/endpoints/root.rb
@@ -1,7 +1,29 @@
+require "json"
+
 module Endpoints
   class Root < Base
     get "/" do
-      "hello."
+      # Display Rdio and Spotify info or initiate OAuth flow
+
+      unless rdio_access_token = env['rack.session'][:rdio_access_token]
+        redirect "/auth/rdio"
+      end
+
+      r = JSON.parse(rdio_access_token.post('http://api.rdio.com/1/',
+        method: 'getPlaylists',
+        extras: '[{ "field": "tracks", "extras": [{ "field": "*", "exclude": true }, {"field": "album"}, {"field": "artist"}, {"field": "duration"}, {"field": "isrcs"}, {"field": "key"}, {"field": "name"}] }]'
+      ).body)
+      "<pre>#{JSON.pretty_generate(r)}</pre>"
+    end
+
+    get "/clear" do
+      env['rack.session'][:rdio_access_token] = nil
+      redirect "/"
+    end
+
+    get "/auth/rdio/callback" do
+      env['rack.session'][:rdio_access_token] = request.env['omniauth.auth']['extra']['access_token']
+      redirect "/"
     end
   end
 end

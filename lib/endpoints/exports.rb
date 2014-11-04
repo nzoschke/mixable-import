ecb7f3fbe45a81f260a0721bd3ddb67ff4b5da0f
Noah Zoschke <nzoschke@gmail.com>
2014-11-04 04:19:40 -0800

    serialize method

--- lib/endpoints/exports.rb
+++ lib/endpoints/exports.rb
@@ -5,6 +5,10 @@ module Endpoints
         content_type :json, charset: 'utf-8'
       end
 
+      def serialize(export)
+        Serializers::Export.new(:default).serialize(export)
+      end
+
       get do
         encode [{ id: "01234567-89ab-cdef-0123-456789abcdef", created_at: '2012-01-01T12:00:00Z', updated_at: '2012-01-01T12:00:00Z', rdio_username: 'nzoschke', state: 'pending' }]
       end
@@ -13,23 +17,23 @@ module Endpoints
         e = Export.create(rdio_username: 'nzoschke', state: 'pending')
         e.save
         status 201
-        encode Serializers::Export.new(:default).serialize(e)
+        encode serialize(e)
       end
 
       get "/:id" do
         e = Export[params[:id]]
-        encode Serializers::Export.new(:default).serialize(e)
+        encode serialize(e)
       end
 
       patch "/:id" do |id|
         e = Export[params[:id]]
-        encode Serializers::Export.new(:default).serialize(e)
+        encode serialize(e)
       end
 
       delete "/:id" do |id|
         e = Export[params[:id]]
         e.delete
-        encode Serializers::Export.new(:default).serialize(e)
+        encode serialize(e)
       end
     end
   end

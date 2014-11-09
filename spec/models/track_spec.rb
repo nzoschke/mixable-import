e7c4f5d795e7f2f7ee1be80cf394bcc114d0a8f2
Noah Zoschke <nzoschke@gmail.com>
2014-11-08 18:58:08 -0800

    matchers spec

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -10,4 +10,17 @@ describe Track do
     assert_equal({ :isrc=>"GBARA9800069", :artist=>"Portishead", :album=>"Roseland NYC Live", :name=>"Glory Box (Live)", :duration=>337 }, t.get_rdio)
     assert_equal({ :isrc=>"GBARA9800069", :artist=>"Portishead", :album=>"Roseland NYC Live", :name=>"Glory Box - Live", :duration=>337 }, t.get_spotify)
   end
+
+  # TODO: test matchers via stubs/mocks
+  it "matches an rdio_key to a spotify_id by first result" do
+    t = Track[rdio_key: "t3160205"]
+    m = t.match_by_first_result
+    assert_equal({ "4XuHSTkpToHdZgxp0xar6i" => { :isrc=>"GBARA9800069", :artist=>"Portishead", :album=>"Roseland NYC Live", :name=>"Glory Box - Live", :duration=>337 } }, m)
+  end
+
+  it "matches an rdio_key to a spotify_id by total edit distance" do
+    t = Track[rdio_key: "t3160205"]
+    m = t.match_by_total_edit_distance
+    assert_equal({ "4XuHSTkpToHdZgxp0xar6i" => { :isrc=>"GBARA9800069", :artist=>"Portishead", :album=>"Roseland NYC Live", :name=>"Glory Box - Live", :duration=>337 } }, m)
+  end
 end

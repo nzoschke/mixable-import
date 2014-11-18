ec4fd8d09d7e158fd1a944bd96243523792f8f38
Noah Zoschke <noah@heroku.com>
2014-11-18 06:35:26 -0800

    worker instrumentation

--- lib/worker.rb
+++ lib/worker.rb
@@ -1,3 +1,45 @@
+module Sidekiq::Middleware::Server
+  class Logging
+    def call(worker, item, queue)
+      cls = worker.class.to_s.downcase
+
+      Sidekiq::Logging.with_context("#{worker.class.to_s} JID-#{item['jid']}") do
+        begin
+          start = Time.now
+
+          # TODO: Can we pass `jid` to the worker function, Pliny REQUEST_ID style?
+          Pliny.log(
+            jid:      item["jid"],
+            "#{cls}"  => true,
+            at:       "start",
+            queue:    item["queue"],
+          )
+
+          yield
+
+          Pliny.log(
+            jid:      item["jid"],
+            "#{cls}"  => true,
+            at:       "finish",
+            queue:    item["queue"],
+            elapsed:  (Time.now - start).to_f
+          )
+        rescue Exception
+          Pliny.log(
+            jid:      item["jid"],
+            "#{cls}"  => true,
+            at:       "exception",
+            queue:    item["queue"],
+            elapsed:  (Time.now - start).to_f
+          )
+          raise
+        end
+      end
+    end
+  end
+end
+
+
 class UserPlaylistsWorker
   include Sidekiq::Worker
 

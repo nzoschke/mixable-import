e2380110a30d7dd152176add997d595b3b19a129
Noah Zoschke <noah@heroku.com>
2014-12-05 06:00:21 -0800

    add playlists to serializer properties

--- lib/serializers/import.rb
+++ lib/serializers/import.rb
@@ -4,6 +4,7 @@ class Serializers::Import < Serializers::Base
       created_at: arg.created_at.try(:iso8601),
       id:         arg.uuid,
       updated_at: arg.updated_at.try(:iso8601),
+      playlists:  arg.spotify_playlists,
     }
   end
 end

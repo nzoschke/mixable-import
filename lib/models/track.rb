fa686d16231d80358ab5b808d31d160930dcce7c
Noah Zoschke <noah@heroku.com>
2014-12-07 19:55:49 -0800

    bugfix, rdio can report null isrcs

--- lib/models/track.rb
+++ lib/models/track.rb
@@ -50,7 +50,7 @@ class Track < Sequel::Model
     end
 
     update(
-      rdio_isrcs:           "{#{rdio_isrcs.join(',')}}",
+      rdio_isrcs:           "{#{rdio_isrcs.compact.join(',')}}",
       spotify_id:           spotify_id,
       spotify_name:         match[:name],
       spotify_album:        match[:album],
